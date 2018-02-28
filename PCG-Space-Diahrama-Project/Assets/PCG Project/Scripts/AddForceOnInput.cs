304ab5754be282ab855ba0edd183e1f3d911c6ff
MeguminIndex <MeguminIndex@users.noreply.github.com>
2018-02-28 23:06:54 +0000

    Basic Astorid Rules
    Added Spawn groupings
    Added Astroid variation support
    Added Detail Distance models for some astroids (LOD)

---
+++ PCG-Space-Diahrama-Project/Assets/PCG Project/Scripts/AddForceOnInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForceOnInput : MonoBehaviour {
+
+
+    public float force;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.forward * force);
+        }
+
+		
+	}
+}
--- PCG-Space-Diahrama-Project/Assets/PCG Project/Scripts/AsteroidController.cs
+++ PCG-Space-Diahrama-Project/Assets/PCG Project/Scripts/AsteroidController.cs
@@ -1,41 +1,82 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class AsteroidController : MonoBehaviour {
 
     public Vector3 spawnArea;
+    public int numPoints;
+    public float mod;
     public float density;
-    private int numstroids;
-    private List<GameObject> astroids;
+
+    public Vector3 sizeMin;
+    public Vector3 sizeMax;
+
+    public int astroidsBeforeVariation;
+
+
 
     [SerializeField]
     private GameObject[] astroidPrefabs;
 
-	// Use this for initialization
-	void Start () {
+    private int numstroids;
+    private List<GameObject> astroids;
+
+    private void Start()
+    {
+        InitAsteroids();
+    }
+
+
+    public void InitAsteroids()
+    {
         astroids = new List<GameObject>();
 
-        InitAsteroids();
+        numstroids = (int)(spawnArea.sqrMagnitude * density);
+        Debug.Log(numstroids);
+        astroids.Capacity = numstroids;
 
-    }
-	
-	
-
-    void InitAsteroids()
-    {
-
-        numstroids = (int)(spawnArea.magnitude * density);
-
-        for (int i =0; i < numstroids; i++)
+        //generate spawn point areas (groups), asteriods tend to group up a bit so having several base points for them to gravitate around good
+        Vector3[] points = new Vector3[numPoints];
+        for(int i =0; i < numPoints; i++)
         {
-            GameObject astroid = GameObject.Instantiate(astroidPrefabs[0]);
+            //generate these points randomly so the layout could be different
+            points[i] = new Vector3(Random.Range(-spawnArea.x, spawnArea.x),
+                Random.Range(-spawnArea.y, spawnArea.y),
+                Random.Range(-spawnArea.z, spawnArea.z));
+        }
+                                     
+        for (int i =0; i <= numstroids; i++)
+        {
+            byte val = 0;
+            if( i % astroidsBeforeVariation == 0)
+            {
+                val = (byte)Random.Range(0, astroidPrefabs.Length);
+            }
 
-            float x =0, y=0,z=0;
+          
+            GameObject astroid = GameObject.Instantiate(astroidPrefabs[val]);
 
-            x = Random.Range(-spawnArea.x, spawnArea.x);
-            y = Random.Range(-spawnArea.y, spawnArea.y);
-            z = Random.Range(-spawnArea.z,spawnArea.z);
+            float x = 0, y = 0, z = 0;
+
+            x = Random.Range(sizeMin.x, sizeMax.x);
+            y = Random.Range(sizeMin.y, sizeMax.y);
+            z = Random.Range(sizeMin.z, sizeMax.z);
+
+
+
+            astroid.transform.localScale = new Vector3(x,y,z);
+
+            int pointInd = Random.Range(0, numPoints);
+            
+            //astroids position based on a random basepoint + a random offset from that point
+            x = Random.Range(points[pointInd].x + (-spawnArea.x/ mod), points[pointInd].x +(spawnArea.x / mod));
+            y = Random.Range(points[pointInd].y +(-spawnArea.y/ mod), points[pointInd].y +(spawnArea.y / mod));
+            z = Random.Range(points[pointInd].z + (-spawnArea.z / mod), points[pointInd].z +(spawnArea.z / mod));
+
+            //take into consideration ships ect at some point??
+
 
             Vector3 pos = new Vector3(x,y,z);
             Quaternion rot = Random.rotation;
@@ -43,12 +84,23 @@ public class AsteroidController : MonoBehaviour {
             astroid.transform.rotation = rot;
             astroids.Add(astroid);
 
+          
+
         }
+
+
+        
 
 
 
     }
 
 
+
+    private void OnApplicationQuit()
+    {
+        astroids.Clear();
+        astroids = null;
+    }
 
 }
