86ae3ea87b556ee197f07b00bb44ee5fdacc75ac
MeguminIndex <MeguminIndex@users.noreply.github.com>
2018-04-17 11:11:15 +0100

    Planet Colour PCG
    Red and blue colour changes based on distance from the closest sun  within a givven range.

--- PCG-Space-Diahrama-Project/Assets/PCG Project/Scripts/PlanetGenerator.cs
+++ PCG-Space-Diahrama-Project/Assets/PCG Project/Scripts/PlanetGenerator.cs
@@ -7,6 +7,13 @@ public class PlanetGenerator : MonoBehaviour
 
     MeshRenderer rend;
     Material mat;
+
+    public float maxRedWeightDist;
+    public float maxBlueWeight;
+   // public float 
+   
+
+
     // Use this for initialization
     void Start()
     {
@@ -16,7 +23,30 @@ public class PlanetGenerator : MonoBehaviour
         mat = new Material(rend.material);
 
         //create random colour
-        Color tmpCol = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);//make it so colour is reder when closer to a sun?
+        Color tmpCol= Random.ColorHSV(0f, 1f, 1f, 1f, 0.0f, 1f);//make it so colour is reder when closer to a sun?
+
+        //tmpCol = Color.black;
+
+
+        float closestSunDist = float.MaxValue;
+        float tmpDIst;
+        GameObject[] suns = GameObject.FindGameObjectsWithTag("Sun");
+        for(int i =0; i < suns.Length; i++)
+        {
+            tmpDIst = Vector3.Distance(transform.position, suns[i].transform.position);
+            if (tmpDIst < closestSunDist)
+                closestSunDist = tmpDIst;
+             
+        }
+
+
+      
+        tmpCol.r = Mathf.InverseLerp(maxRedWeightDist, 0.0f, closestSunDist);
+        tmpCol.b = Mathf.InverseLerp(maxRedWeightDist, maxBlueWeight, closestSunDist);
+      
+        tmpCol.a = 1;
+      
+
         Debug.Log("Planet Colour: " + tmpCol.ToString());
 
         mat.color = tmpCol;//update mat colour
